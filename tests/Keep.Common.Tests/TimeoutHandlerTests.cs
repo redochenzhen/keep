d9309f092df535fba326f82ad18f64ec304460de
Zhen Chen <redo.chenzhen@hotmail.com>
2021-02-22 15:05:09 +0800

    1.0.0-beta1

--- tests/Keep.Common.Tests/TimeoutHandlerTests.cs
+++ tests/Keep.Common.Tests/TimeoutHandlerTests.cs
@@ -1,6 +1,7 @@
 using Keep.Common.Http;
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -26,10 +27,23 @@ namespace Keep.Common.Tests
             var http = new HttpClient(TimeoutHandler.Create());
             http.Timeout = TimeSpan.FromMilliseconds(1);
             var request = new HttpRequestMessage(HttpMethod.Get, "http://baidu.com");
-            request.SetTimeout(TimeSpan.FromMilliseconds(10));
+            request.SetTimeout(TimeSpan.FromMilliseconds(100));
             await Assert.ThrowsAsync<TaskCanceledException>(async () =>
             {
                 await http.SendAsync(request);
+            });
+        }
+
+        [Fact]
+        public async void CanceledByUserTests()
+        {
+            var http = new HttpClient(TimeoutHandler.Create());
+            http.Timeout = TimeSpan.FromMilliseconds(100);
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://baidu.com");
+            var cts = new CancellationTokenSource(1);
+            await Assert.ThrowsAsync<TaskCanceledException>(async () =>
+            {
+                await http.SendAsync(request, cts.Token);
             });
         }
     }
